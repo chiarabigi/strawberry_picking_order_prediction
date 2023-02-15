1ed83137a553bcd9ffc9a270f2d82a6901ea4870
chiarabigi <chiara.bigi@mail.polimi.it>
2023-02-15 15:16:32 +0000

    add

---
+++ config_scheduling.py
@@ -0,0 +1,19 @@
+from dataset import SchedulingDataset
+from model import GCN_scheduling
+from utils import BatchAccuracy_scheduling
+
+LR = 1e-3
+HL = 8
+BATCHSIZE = 32
+NL = 0
+WEIGHTDECAY = 0.01
+SEEDNUM = 0
+TORCHSEED = 0
+
+ACCURACY = BatchAccuracy_scheduling
+
+DATASET = SchedulingDataset
+
+MODEL = GCN_scheduling(HL, NL)
+
+
